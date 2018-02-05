89a121c661daad95b0f6b90648beaff0b1e354ca
Ruhan.Odendaal <ruhan.odendaal@investec.co.za>
2018-02-05 09:28:32 +0200

    ngx-carousel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,8 +3,9 @@ import { NgModule } from '@angular/core';
 
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
-import { FlexLayoutModule } from "@angular/flex-layout";
+import {FlexLayoutModule} from '@angular/flex-layout';
 import { MaterialModule } from './material.module';
+import { NgxCarouselModule } from 'ngx-carousel';
 import { AppComponent } from './app.component';
 import { IntroComponent } from './intro/intro.component';
 import { HeaderComponent } from './header/header.component';
@@ -21,7 +22,8 @@ import { HeaderComponent } from './header/header.component';
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    NgxCarouselModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 // import { ActivatedRoute } from "@angular/router"; //This is when you send through variables through url
+import { NgxCarousel, NgxCarouselStore  } from 'ngx-carousel';
 
 @Component({
   selector: 'app-intro',
@@ -7,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./intro.component.scss']
 })
 export class IntroComponent implements OnInit {
+
+  public carouselBanner: NgxCarousel;
 
   constructor() { }
 
@@ -16,6 +19,50 @@ export class IntroComponent implements OnInit {
     btn.addEventListener('click', function () {
       alert('hello');
     });
+
+    this.carouselBanner = {
+      grid: {xs: 1, sm: 1, md: 1, lg: 1, all: 0},
+      slide: 1,
+      speed: 400,
+      interval: 4000,
+      point: {
+        visible: true,
+        pointStyles: `
+          .ngxcarouselPoint {
+            list-style-type: none;
+            text-align: center;
+            padding: 12px;
+            margin: 0;
+            white-space: nowrap;
+            overflow: auto;
+            position: absolute;
+            width: 100%;
+            bottom: 20px;
+            left: 0;
+            box-sizing: border-box;
+          }
+          .ngxcarouselPoint li {
+            display: inline-block;
+            border-radius: 999px;
+            background: rgba(255, 255, 255, 0.55);
+            padding: 5px;
+            margin: 0 3px;
+            transition: .4s ease all;
+          }
+          .ngxcarouselPoint li.active {
+              background: white;
+              width: 10px;
+          }
+        `
+      },
+      load: 2,
+      loop: true,
+      touch: true
+    };
+  }
+
+  onmoveFn(data: NgxCarouselStore) {
+    console.log(data);
   }
 
 }
